232f7339b431f342cf98fdd19a172037576580cd
Aayansh Athaghara <aayansh.ath@gmail.com>
2026-07-06 10:48:30 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+blocks.onBlockPlaced(DIRT, function () {
+    gameplay.setWeather(THUNDER)
+    gameplay.setDifficulty(HARD)
+    gameplay.setGameMode(
+    SURVIVAL,
+    mobs.target(ALL_PLAYERS)
+    )
+    music.playSound(Sound.Explode)
+})
+player.onChat("Herobrine", function () {
+	
+})
+player.onChat("Spawn 1212", function () {
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WARDEN), pos(0, 0, 0))
+    mobs.spawn(mobs.monster(WITHER_MONSTER_MOB), pos(0, 0, 0))
+})
+player.onChat("Hi", function () {
+	
+})
